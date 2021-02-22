5b02f4c35977c661730da4b971507956cb8fcf9d
Alexej Kloos <alexejkloos@gmail.com>
2021-02-22 13:23:27 +0100

    wip

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace IndustyArena\CraftObjectStorage;
+
+use craft\events\RegisterComponentTypesEvent;
+use craft\services\Volumes;
+use yii\base\Event;
+
+class Plugin extends \craft\base\Plugin
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // Event::on(Volumes::class, Volumes::EVENT_REGISTER_VOLUME_TYPES, function (RegisterComponentTypesEvent $event) {
+        //     $event->types[] = Volume::class;
+        // });
+    }
+}
